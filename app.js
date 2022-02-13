5b6d5a4c0eac20c8841373cc411aded7bf05f0a1
arefin6 <imranhchy@gmail.com>
2022-02-13 23:45:36 +0600

    Calculator implemented

--- app.js
+++ app.js
@@ -11,3 +11,17 @@ document.getElementById("generate-pin").addEventListener("click", function () {
   const pin = getPin();
   document.getElementById("display-pin").value = pin;
 });
+
+document.getElementById("key-pad").addEventListener("click", function (event) {
+  const numbers = event.target.innerText;
+  const calcInput = document.getElementById("typed-numbers");
+  if (isNaN(numbers)) {
+    if (numbers == "C") {
+      calcInput.value = "";
+    }
+  } else {
+    const previousNumber = calcInput.value;
+    const newNumber = previousNumber + numbers;
+    calcInput.value = newNumber;
+  }
+});
